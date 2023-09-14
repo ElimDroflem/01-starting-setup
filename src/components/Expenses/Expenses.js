7d38816e0c98882a2c7346edb355eb57fab409f2
ElimDroflem <coreyr103@hotmail.co.uk>
2023-09-14 23:49:34 +0100

    made the adding of expenses dynamic with map function

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -19,30 +19,14 @@ export default function Expenses(props) {
           selected={selectedYear}
           onYearChange={handleYearChange}
         />
-        <ExpenseItem
-          id={props.items[0].id}
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          id={props.items[1].id}
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          id={props.items[2].id}
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          id={props.items[3].id}
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
